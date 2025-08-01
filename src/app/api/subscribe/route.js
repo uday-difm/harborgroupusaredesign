0938e4c9931569ffe39d5696c604bbf9c591b19e
anishayadav1996 <anishaiert@gmail.com>
2025-08-01 07:46:51 -0400

    updated subscribe email

--- src/app/api/subscribe/route.js
+++ src/app/api/subscribe/route.js
@@ -3,8 +3,8 @@
 "use server";
 // import db from "../../../../lib/mysql";
 import pool from "../../../../lib/mysql";
-// import { sendMail } from "../../../../lib/nodemailer";
-// import { generateEmailTemplate } from "../../../../lib/emailTemplate";
+import { sendMail } from "../../../../lib/nodemailer";
+import { generateEmailTemplate } from "../../../../lib/emailTemplate";
 
 export async function POST(request) {
   try {
@@ -36,51 +36,51 @@ export async function POST(request) {
     const [result] = await pool.query(query, values);
 
     // Generate email content using the common template
-//     const emailContent = generateEmailTemplate({
-//      subject: "🎉 Welcome to 1stAlliance Group – You're Subscribed!",
-//   body: `
-//   <div style="font-family: 'Helvetica Neue', sans-serif; max-width: 600px; margin: auto; color: #2c3e50;">
-//     <h2 style="color: #008fa3;">Welcome to 1stAlliance Group!</h2>
+    const emailContent = generateEmailTemplate({
+     subject: "🎉 Welcome to HarborGroupUSA – You're Subscribed!",
+  body: `
+  <div style="font-family: 'Helvetica Neue', sans-serif; max-width: 600px; margin: auto; color: #2c3e50;">
+    <h2 style="color: #008fa3;">Welcome to HarborGroupUSA!</h2>
 
-//     <p style="font-size: 16px;">
-//       Thank you for subscribing. You're now part of a community that values innovation, trust, and professional growth in the financial and business landscape.
-//     </p>
+    <p style="font-size: 16px;">
+      Thank you for subscribing. You're now part of a community that values innovation, trust, and professional growth in the financial and business landscape.
+    </p>
 
-//     <h3 style="color: #1eaae2;">Here’s what you can expect:</h3>
-//     <ul style="font-size: 14px; line-height: 1.6;">
-//       <li>💼 Exclusive insights into business strategy and finance</li>
-//       <li>📢 Announcements on services and offers</li>
-//       <li>📘 Resources and tools to grow your impact</li>
-//     </ul>
+    <h3 style="color: #1eaae2;">Here’s what you can expect:</h3>
+    <ul style="font-size: 14px; line-height: 1.6;">
+      <li>💼 Exclusive insights into business strategy and finance</li>
+      <li>📢 Announcements on services and offers</li>
+      <li>📘 Resources and tools to grow your impact</li>
+    </ul>
 
-//     <p style="font-size: 16px;">
-//       We’re excited to have you on board. Expect your first newsletter soon!
-//     </p>
+    <p style="font-size: 16px;">
+      We’re excited to have you on board. Expect your first newsletter soon!
+    </p>
 
-//     <div style="margin-top: 30px; font-size: 14px; color: #7f8c8d;">
-//       <hr style="border: 1px solid #ecf0f1;" />
-//       <p>
-//         If you have any questions or would like to reach out, visit our 
-//         <a href="https://1stalliancegrp.com/contact-us" style="color: #1eaae2; text-decoration: none;">contact page</a>.
-//       </p>
-//       <p>
-//         Best regards,<br />
-//         <strong>The 1stAlliance Group Team</strong><br />
-//         <i>Empowering financial excellence</i>
-//       </p>
-//     </div>
-//   </div>
-//   `,
-//   footer: "© 2025 1stAlliance Group. All rights reserved.",
-//     });
+    <div style="margin-top: 30px; font-size: 14px; color: #7f8c8d;">
+      <hr style="border: 1px solid #ecf0f1;" />
+      <p>
+        If you have any questions or would like to reach out, visit our 
+        <a href="https://1stalliancegrp.com/contact-us" style="color: #1eaae2; text-decoration: none;">contact page</a>.
+      </p>
+      <p>
+        Best regards,<br />
+        <strong>The HarborGroupUSA Team</strong><br />
+        <i>Empowering financial excellence</i>
+      </p>
+    </div>
+  </div>
+  `,
+  footer: "© 2025 HarborGroupUSA. All rights reserved.",
+    });
 
 
-    // Send the email to the user
-    // await sendMail({
-    //   to: email,
-    //   subject: "🎉 You're subscribed to 1stAlliance Group",
-    //   html: emailContent,
-    // });
+    //Send the email to the user
+    await sendMail({
+      to: email,
+      subject: "🎉 You're subscribed to HarborGroupUSA",
+      html: emailContent,
+    });
 
     return new Response(
       JSON.stringify({
