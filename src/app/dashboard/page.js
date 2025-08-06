57a1644f2f3cccffff7140f02e55e6afca9258cb
anishayadav1996 <anishaiert@gmail.com>
2025-08-06 04:48:50 -0500

    update authentic

--- src/app/dashboard/page.js
+++ src/app/dashboard/page.js
@@ -22,7 +22,7 @@ export default function DashboardHome() {
  useEffect(() => {
   const fetchUserData = async () => {
     try {
-      const response = await fetch('/api/dashboard/user', {
+      const response = await fetch('/api/dashboard/checkauth', {
         method: 'GET',
         credentials: 'include' // includes cookies in request
       });
@@ -30,7 +30,7 @@ export default function DashboardHome() {
       if (response.ok) {
         const data = await response.json();
         console.log("Fetched user data:", data);
-        setUser(data);
+        setUser(data.user);
       } else if (response.status === 401 || response.status === 403) {
         router.push('/dashboard/login');
       } else {
