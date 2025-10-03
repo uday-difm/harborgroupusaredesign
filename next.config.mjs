343b35695fb0f74039100f44e9e3c223ecc666ca
pryadav@123 <yprem0092@gmail.com>
2025-10-03 01:13:23 -0400

    next.config.mjs updated

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  api: {
-    bodyParser: {
-      sizeLimit: '50mb',
-    },
-  },
   reactStrictMode: true,
   images: {
-    domains: ['harborgroupusa.s3-eu-central-2.ionoscloud.com'],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'harborgroupusa.s3-eu-central-2.ionoscloud.com',
+        pathname: '/**',
+      },
+    ],
   },
 };
 
